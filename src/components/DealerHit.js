4680fa3de82689ccf70719305d5eb5cc1d888487
bdnkai <bdn.kai@gmail.com>
2022-06-22 08:20:06 -0700

    dealer hit is fully functional

--- src/components/DealerHit.js
+++ src/components/DealerHit.js
@@ -1,21 +1,57 @@
-import React from 'react';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { gameContext } from './context/gameContext';
+import DealerRendersHit from './DealerRendersHit';
 
 function DealerHit() {
-	console.log('dealer is read');
-	const [currentCard, setCurrentCard] = useState();
+	const [dealerHit, setDealerHit] = useState();
+	const [dealerCode, setDealerCode] = useState();
 	const { bJ, setBJ } = useContext(gameContext);
-	const newSetURL = `https://deckofcardsapi.com/api/deck/${bJ.deck}/draw/?count=1`;
+	let isMounted = useRef(true);
 
-	const fetchSet = () => {
-		fetch(newSetURL)
-			.then((res) => res.json())
+	const fetchCard = () => {
+		const newCardURL = `https://deckofcardsapi.com/api/deck/${bJ.deck}/draw/?count=1`;
+
+		fetch(newCardURL)
 			.then((res) => {
-				setCurrentCard(res.cards);
+				return res.json();
+			})
+			.then((res) => {
+				console.log(res);
+				setTimeout(() => {
+					setDealerHit(res.cards);
+				}, 200);
 			});
 	};
-	fetchSet();
+	const dealCard = () => {
+		const dHand = `${[dealerHit[0].code]}`;
+		const dealerURL = `https://deckofcardsapi.com/api/deck/${bJ.deck}/pile/dealer/add/?cards=${dHand}`;
+		fetch(dealerURL)
+			.then((res) => {
+				return res.json();
+			})
+			.then((res) => {
+				setTimeout(() => {
+					setDealerCode('success');
+					console.log(`${dealerCode}`);
+				}, 200);
+			});
+	};
+	useEffect(() => {
+		if (isMounted) {
+			setTimeout(() => {
+				fetchCard();
+			}, 200);
+		}
+		isMounted = false;
+	}, []);
+
+	useEffect(() => {
+		{
+			dealerHit && dealCard();
+		}
+	}, [fetchCard]);
+
+	return <div>{dealerCode && <DealerRendersHit />}</div>;
 }
 
 export default DealerHit;
